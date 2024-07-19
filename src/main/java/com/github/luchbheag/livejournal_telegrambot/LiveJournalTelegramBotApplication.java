d1b75081f8f506c04f99c55a7d50494a86425a18
luchbheag <nedolivko.julia@gmail.com>
2024-07-19 19:39:01 +0200

    STEP-0: add SpringBoot skeleton for the project

---
+++ src/main/java/com/github/luchbheag/livejournal_telegrambot/LiveJournalTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.luchbheag.livejournal_telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveJournalTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveJournalTelegramBotApplication.class, args);
+	}
+
+}
