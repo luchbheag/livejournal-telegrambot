dd1fde5cd6c38821f799338c1486f645124ec4fc
luchbheag <nedolivko.julia@gmail.com>
2024-07-27 16:06:04 +0200

    STEP-2: add stub telegram bot to the project

--- src/main/java/com/github/luchbheag/livejournal_telegrambot/LiveJournalTelegramBotApplication.java
+++ src/main/java/com/github/luchbheag/livejournal_telegrambot/LiveJournalTelegramBotApplication.java
@@ -2,8 +2,13 @@ package com.github.luchbheag.livejournal_telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.github.luchbheag.livejournal_telegrambot",
+		"org.telegram.telegrambots"
+})
 public class LiveJournalTelegramBotApplication {
 
 	public static void main(String[] args) {
